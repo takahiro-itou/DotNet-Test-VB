03d456a68507aff8228ac31c93fb3a1da39a2f84
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-07-23 17:18:56 +0900

    古いフレームワークでも動くように修正

--- Sample/TestVB/Program.vb
+++ Sample/TestVB/Program.vb
@@ -2,7 +2,6 @@
 
     <STAThread()>
     Friend Sub Main(args As String())
-        Application.SetHighDpiMode(HighDpiMode.SystemAware)
         Application.EnableVisualStyles()
         Application.SetCompatibleTextRenderingDefault(False)
         Application.Run(New Form1)
